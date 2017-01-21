a2d67f80ec898872b06d56397fe5b579a2f3ce08
Blanca Mikyle Ciencia Bello <mikyleciencia@gmail.com>
2017-01-21 11:10:14 +0800

    Create linkedlist_stack.cpp

---
+++ linkedlist_stack.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class Stack {
+  public:
+    Stack(int num) {
+      top = 0;
+      maxelem = num;
+      s = new int[maxelem];
+    }
+    
+  void push(int t) {
+  	cout << "Push "<<t<<": \n";
+    if (top == maxelem) 
+		return;
+    s[top++] = t;
+  }
+  
+  int pop() {
+  	cout << "Pop: \n";
+    if (top == 0) 
+		return -1;
+    return s[--top];
+  }
+  
+  void display() {
+    if (top == 0) {
+      cout << "(empty)\n";
+      return;
+    }
+    for (int t = 0; t < top; t++) 
+		cout << s[t] << " ";
+    cout << "\n";
+  }
+  
+  int empty() {
+    return top == 0;
+  }
+  
+  private:
+    int * s;
+  int top;
+  int maxelem;
+};
+
+void menu(){
+	cout << "What would you like to do?: \n";
+	cout << "1: Push \n";
+	cout << "2: Pop \n";
+	cout << "3: Display \n";
+	cout << "4: Exit \n";
+}
+
+int main() {
+  int choice, psh ;
+  Stack * s = new Stack(100);
+  while(1){
+  system("cls");
+  menu();
+  cout<<"Enter Choice: ";
+  cin >> choice;
+  
+  switch (choice){
+  	case 1:{
+  		cout<< "Number to PUSH: ";
+  		cin>> psh;
+  		s->push(psh);
+		  break;
+	  }
+	  case 2:{
+		s->pop();
+		break;
+	  }
+	  case 3:{
+		s->display();
+		break;
+	  }
+	  case 4:{
+		exit(1);
+		break;
+	  }
+	  default:{
+		cout<<"Invalid Choice";
+		break;
+		}
+  }
+  	getch();
+  }
+  return 1;
+}
