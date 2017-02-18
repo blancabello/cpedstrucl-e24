b44e521b728d9823773e62c70fb7b50770abc0a1
Blanca Mikyle Ciencia Bello <mikyleciencia@gmail.com>
2017-02-18 12:36:36 +0800

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int j, burp(1);
+	int A[]={40,30,20,50,10};
+	
+	for(int i=1;i<5;i++){
+		int tmp=A[i];
+		for(j=i-1;j>=0 && tmp<A[j];j--){
+			A[j+1] = A[j];
+		}
+		A[j+1] = tmp;
+			
+			cout<<"Pass "<<++burp<<endl;
+			for(int f=0;f<5;++f)
+			cout<<" "<<A[f]<<endl;
+	}
+	
+}
